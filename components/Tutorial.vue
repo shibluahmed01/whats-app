3e31d02eb5c6b4fbb8eab9af7f01480b8c1843c8
shibluahmed01 <mdshibluahmed01@gmail.com>
2022-09-12 13:08:11 -1200

    first commit

---
+++ components/Tutorial.vue
@@ -0,0 +1,632 @@
+<template>
+  <div class="body">
+    <div class="sidebar">
+      <div class="header">
+        <div class="avatar">
+          <img src="../img/lisa.jpg" alt="" />
+        </div>
+        <div class="chat-header-right">
+          <img src="../img/refresh.png" alt="" />
+          <img src="../img/message.png" alt="" />
+          <img src="../img/3dot.png" alt="" />
+        </div>
+      </div>
+      <div class="sidebar-search">
+        <div class="sidebar-search-container">
+          <img src="../img/search.png" alt="" />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div class="sidebar-chats">
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Lima Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Lisa Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Justin Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Rimon Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Shadin Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Lamyea Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Sagor Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Rima Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Asik Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Merajul Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Robin Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>DJ Khalad Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Raihan Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Fahim Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Muntasir Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Sabbir Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Brad Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Max Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Mim Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Sara Daisan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Khalad khan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Rana Rohman</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Nerob Sheak</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+        <div class="sidebar-chat">
+          <div class="chat-avatar">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="chat-info">
+            <h4>Rabbi khan</h4>
+            <p>Last Message</p>
+          </div>
+          <div class="time">
+            <p>2.44 pm</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="message-container">
+      <div class="header">
+        <div class="chat-title">
+          <div class="avatar {">
+            <img src="../img/lisa.jpg" alt="" />
+          </div>
+          <div class="message-header-content">
+            <h4>Lisa Daisan</h4>
+            <p>online</p>
+          </div>
+        </div>
+        <div class="chat-header-right">
+          <img class="pic" src="../img/search.png" alt="" />
+          <img src="../img/3dot.png" alt="" />
+        </div>
+      </div>
+      <div class="message-content">
+        <p class="chat-message">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message chat-sent">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message chat-sent">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message chat-sent">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message chat-sent">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message chat-sent">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message chat-sent">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message chat-sent">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+        <p class="chat-message">
+          This is a message <span class="chat-timestamp">11:30 pm</span>
+        </p>
+      </div>
+      <div class="message-footer">
+        <img src="../img/smile.png" alt="" />
+        <img src="../img/paper.png" alt="" />
+        <input type="text" placeholder="Type a message" />
+        <img src="../img/microphone.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: #303030;
+  font-family: "roboto", sans-serif;
+}
+
+::-webkit-scrollbar {
+  width: 6px;
+}
+
+::-webkit-scrollbar-track {
+  background: none;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #b9b3ae;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+
+.header .chat-header-right .pic {
+  margin-left: 10rem;
+}
+
+.avatar {
+  position: relative;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.avatar img,
+.chat-avatar img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.chat-header-right {
+  display: flex;
+  width: 20%;
+  height: 100%;
+  margin: 5px 13px;
+  opacity: 0.4;
+}
+
+.body {
+  display: flex;
+  overflow: hidden;
+}
+
+.sidebar {
+  flex: 5.17;
+}
+
+.header {
+  background-color: #ededed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 15px;
+  border-right: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.chat-header-right img {
+  width: 28px;
+  margin-left: 1rem;
+}
+
+.sidebar-search {
+  display: flex;
+  align-items: center;
+  background-color: #f6f6f6;
+  padding: 8px 15px;
+}
+
+.sidebar-search-container {
+  display: flex;
+  align-items: center;
+  background-color: white;
+  width: 100%;
+  height: 35px;
+  border-radius: 20px;
+}
+
+.sidebar-search-container img {
+  width: 15px;
+  height: 15px;
+  margin: 0 15px;
+  opacity: 0.4;
+}
+
+.sidebar-search-container input {
+  border: none;
+  outline: none;
+  margin-left: 20px;
+}
+
+.chat-avatar {
+  position: relative;
+  height: 50px;
+  min-width: 50px;
+  border-radius: 50%;
+  overflow: hidden;
+  flex: 1;
+}
+
+.sidebar-chats {
+  height: calc(100vh - 155px);
+  overflow-y: scroll;
+}
+
+.sidebar-chat {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 10px 12px;
+  width: 100%;
+}
+
+.sidebar-chat:hover {
+  background-color: #f5f5f5;
+}
+
+.sidebar-chat:nth-child(2) {
+  background-color: #ebebeb;
+}
+
+.sidebar-chat::before {
+  content: "";
+  position: absolute;
+  width: 80%;
+  bottom: 0;
+  left: 80px;
+  border-bottom: 1px solid #999;
+  opacity: 0.2;
+}
+
+.chat-info {
+  margin-left: 15px;
+  flex: 10;
+}
+
+.chat-info p {
+  font-size: 13px;
+  line-height: 20px;
+  color: gray;
+}
+
+.time {
+  flex: 1;
+}
+
+.time p {
+  white-space: nowrap;
+  font-size: 13px;
+  margin-top: -16px;
+  color: gray;
+}
+
+.message-container {
+  flex: 12;
+}
+
+.chat-title {
+  display: flex;
+  align-items: center;
+}
+
+.message-header-content {
+  margin-left: 15px;
+}
+
+h4 {
+  font-weight: 400px;
+}
+
+.message-header-content p {
+  font-size: 13px;
+  color: gray;
+}
+
+.message-content {
+  background-image: url(../img/background.PNG);
+  background-position: center;
+  height: calc(100vh - 120px);
+  padding: 20px 80px;
+  overflow-y: scroll;
+}
+
+.chat-message {
+  font-size: 14px;
+  padding: 8px;
+  width: fit-content;
+  border-radius: 8px;
+  background-color: white;
+  margin-bottom: 30px;
+}
+
+.chat-timestamp {
+  margin-left: 10px;
+  font-size: 10px;
+  color: gray;
+}
+
+.chat-sent {
+  margin-left: auto;
+  background-color: #dcf8c6;
+}
+
+.message-footer {
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  background-color: #f0f0f0;
+}
+
+.message-footer img {
+  width: 25px;
+  margin-left: 1rem;
+  height: 25px;
+  opacity: 0.4;
+}
+
+.message-footer input {
+  flex: 20;
+  margin: 5px 10px;
+  padding: 12px 15px;
+  outline: none;
+  border: none;
+  border-radius: 20px;
+  font-size: 15px;
+}
+</style>
